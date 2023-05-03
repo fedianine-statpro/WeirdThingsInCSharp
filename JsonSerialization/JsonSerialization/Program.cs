f4dfe57a55baa8e95055563731f2b88cf8a39f92
Victor Fedianine <vfedianine@confluence.com>
2023-05-03 09:35:45 +0100

    Differences between Newtonsoft.Json and System.Text.Json serialization

---
+++ JsonSerialization/JsonSerialization/Program.cs
@@ -0,0 +1,17 @@
+﻿DerivedClass derivedClass = new DerivedClass{FirstName = "Victor", EmailAddress = "email"};
+BaseClass baseClassValue = derivedClass;
+// Surprisingly even though we are using the base class, the derived class property is serialized with Newtonsoft.Json
+Console.WriteLine(Newtonsoft.Json.JsonConvert.SerializeObject(baseClassValue));
+// Since we are using the base class, the derived class property is not serialized with System.Text.Json (as I would expect)
+Console.WriteLine(System.Text.Json.JsonSerializer.Serialize(baseClassValue));
+Console.ReadLine();
+
+public class BaseClass
+{
+    public string EmailAddress { get; set; }
+}
+
+public class DerivedClass : BaseClass
+{
+    public string FirstName { get; set; }
+}
